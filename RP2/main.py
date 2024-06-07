3ffda827b0be687d2ea384fe2af2be512f67e5e9
Henrik L <helagro@outlook.com>
2024-06-07 19:35:34 +0200

    Add mqtt for backend

--- RP2/main.py
+++ RP2/main.py
@@ -5,17 +5,42 @@ from temperature import getTemperature
 import wifi
 from mqtt import MQTTClient
 
-wifi.connect()
+# ------------------------- VARIABLES ------------------------ #
+
+MOTION_POLL_INTERVAL = 3
+TEMP_POLL_INTERVAL = 30
+
+TEMP_COUNTER_MAX = TEMP_POLL_INTERVAL // MOTION_POLL_INTERVAL
+tempCounter = 0
+
+# ----------------------- PIN SETUP ---------------------- #
 
 LED = Pin("LED", Pin.OUT)
 LED.off()
-client = MQTTClient()
 
+# ----------------------- NETWORK SETUP ---------------------- #
+
+wifi.connect()
+client = MQTTClient()
 client.connect()
 client.publish("test/topic", "RP2 is online")
 
+# ------------------------- MAIN LOOP ------------------------ #
+
 while True:
-    print("Temperature: ", getTemperature(), "Motion?: ", motion.doesDetect())
-    time.sleep(20)
+    if(tempCounter >= TEMP_COUNTER_MAX):
+        client.publish("temperature", str(getTemperature()))
+        tempCounter = 0
+
+    if motion.doesDetect():
+        print("Motion detected!")
+        client.publish("motion", "True")
+        LED.on()
+    else:
+        client.publish("motion", "False")
+        LED.off()
+
+    time.sleep(MOTION_POLL_INTERVAL)
+    tempCounter += 1
 
    
---
+++ backend/mqtt.py
@@ -0,0 +1,19 @@
+import paho.mqtt.client as mqtt
+
+def onMessage(client, userData, msg):
+    if msg.topic == "temperature":
+        print("Temperature:", msg.payload.decode())
+    elif msg.topic.startswith("motion"):
+        print("Motion detected:", msg.payload.decode())
+    else:
+        print("Unknown topic:", msg.topic + ":", msg.payload.decode())
+
+def onConnect(client, userdata, flags, reason_code, properties):
+    client.subscribe("#")
+
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+client.on_connect = onConnect
+client.on_message = onMessage
+
+client.connect("localhost", 1883, 60)
+client.loop_forever()
