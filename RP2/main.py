040d7106c1a83432707fa4c809d32e52ec2fecb8
Henrik L <helagro@outlook.com>
2024-06-07 17:31:05 +0200

    Add temperature sensing

--- RP2/main.py
+++ RP2/main.py
@@ -1,16 +1,13 @@
 import motion
 import time
 from machine import Pin
+from temperature import getTemperature
 
 LED = Pin("LED", Pin.OUT)
 LED.off()
 
 while True:
-    if motion.doesDetect():
-       print("Motion Detected")
-       LED.on()
-       time.sleep(5)
-    else:
-       print("No motion detected -> LED is OFF")
-       LED.off()
-       time.sleep(1)
+    print("Temperature is: ", getTemperature(), "Motion?: ", motion.doesDetect())
+    time.sleep(5)
+
+   
