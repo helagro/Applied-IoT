54a67bd7d0909c85e2ea6dc8a01ec73d4719574d
Henrik L <helagro@outlook.com>
2024-06-08 17:24:30 +0200

    Add get-endpoints to server

--- backend/server.py
+++ backend/server.py
@@ -1,12 +1,39 @@
-from flask import Flask, request, jsonify
-from tradfri import getDevices
+from flask import Flask, jsonify
+from tradfri import getDevices, Action
+from automations import automations
+from values import Sensor
+from compare import Comparator
 
 app = Flask(__name__)
+
+# -------------------------- GET-ENDPOINTS ------------------------- #
 
 @app.route('/api/ikea-devices', methods=['GET'])
 def getIkeaDevices():
     return jsonify(getDevices())
 
+
+@app.route('/api/automations', methods=['GET'])
+def getAutomations():
+    return jsonify(automations)
+
+
+@app.route('/api/sensors', methods=['GET'])
+def getSensors():
+    return jsonify(Sensor)
+
+
+@app.route('/api/comparators', methods=['GET'])
+def getComparators():
+    return jsonify(Comparator)
+
+
+@app.route('/api/actions', methods=['GET'])
+def getActions():
+    return jsonify(Action)
+
+
+# --------------------------- START -------------------------- #
 
 def start():
     print("Starting server", flush=True)
