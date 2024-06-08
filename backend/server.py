ef311adcb7aa78f94b4be695b1e7f80e6615fb92
Henrik L <helagro@outlook.com>
2024-06-08 17:32:01 +0200

    Fix bugs

--- backend/server.py
+++ backend/server.py
@@ -3,6 +3,7 @@ from tradfri import getDevices, Action
 from automations import automations
 from values import Sensor
 from compare import Comparator
+from dataclasses import asdict
 
 app = Flask(__name__)
 
@@ -15,22 +16,32 @@ def getIkeaDevices():
 
 @app.route('/api/automations', methods=['GET'])
 def getAutomations():
-    return jsonify(automations)
+    automationDicts = []
+
+    for automation in automations:
+        automationDict = asdict(automation)
+        automationDict["sensor"] = Sensor(automation["sensor"]).name
+        automationDict["operatorID"] = Comparator(automation["operatorID"]).name
+        automationDict["actionID"] = Action(automation["actionID"]).name
+
+        automationDicts.append(automationDict)
+
+    return jsonify(automationDicts)
 
 
 @app.route('/api/sensors', methods=['GET'])
 def getSensors():
-    return jsonify(Sensor)
+    return jsonify({sensor.name: sensor.value for sensor in Sensor})
 
 
 @app.route('/api/comparators', methods=['GET'])
 def getComparators():
-    return jsonify(Comparator)
+    return jsonify({comparator.name: comparator.value for comparator in Comparator})
 
 
 @app.route('/api/actions', methods=['GET'])
 def getActions():
-    return jsonify(Action)
+    return jsonify({action.name: action.value for action in Action})
 
 
 # --------------------------- START -------------------------- #
