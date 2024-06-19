45c45a9e8e0090fda5e621aa2cc7ee8de2dad66e
Henrik L <helagro@outlook.com>
2024-06-19 20:31:22 +0200

    Better type managment

--- backend/Automation.py
+++ backend/Automation.py
@@ -32,39 +32,39 @@ class Automation:
             self.id = id
 
         if not isinstance(name, str):
-            raise ValueError("Name must be a string")
+            raise ValueError(f"Name must be a string, got {name} of type {type(name)}")
         else:
             self.name = name
         
         if not isinstance(sensor, str):
-            raise ValueError("Sensor must be a string")
+            raise ValueError(f"Sensor must be a string, got {sensor} of type {type(sensor)}")
         else:
             self.sensor = sensor
         
         if not isinstance(operatorID, int):
-            raise ValueError("OperatorID must be an integer")
+            raise ValueError(f"OperatorID must be an integer, got {operatorID} of type {type(operatorID)}")
         else:
             self.operatorID = operatorID
         
         if not isinstance(threshold, float):
-            raise ValueError("Threshold must be an integer")
+            raise ValueError(f"Threshold must be a float, got {threshold} of type {type(threshold)}")
         else:
             self.threshold = threshold
         
         if not isinstance(tradfriDeviceID, int):
-            raise ValueError("TradfriDeviceID must be an integer")
+            raise ValueError(f"TradfriDeviceID must be an integer, got {tradfriDeviceID} of type {type(tradfriDeviceID)}")
         else:
             self.tradfriDeviceID = tradfriDeviceID
         
         if not isinstance(actionID, int):
-            raise ValueError("ActionID must be an integer")
+            raise ValueError(f"ActionID must be an integer, got {actionID} of type {type(actionID)}")
         else:
             self.actionID = actionID
         
         self.actionPayload = actionPayload
         
         if not isinstance(sensorDeviceID, int):
-            raise ValueError("SensorDeviceID must be an integer")
+            raise ValueError(f"SensorDeviceID must be an integer, got {sensorDeviceID} of type {type(sensorDeviceID)}")
         else:
             self.sensorDeviceID = sensorDeviceID
         
